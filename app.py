a80e307be4d5f58f760b13d63e403372d3eb15bb
andrius-siup <andrius.siupinys@gmail.com>
2021-05-04 12:09:28 +0000

    Add Login functionality

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if os.path.exists("env.py"):
     import env
 
 
-app = Flask(__name__)    
+app = Flask(__name__)
 
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -18,6 +18,8 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 # all recipes
+
+
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
@@ -25,6 +27,8 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 # register
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -48,9 +52,33 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)  # before final deployment replace to False
-
-
