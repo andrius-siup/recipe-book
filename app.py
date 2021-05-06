130e0daf422d0563a1e97c8eda49def6bb7eab34
andrius-siup <andrius.siupinys@gmail.com>
2021-05-06 14:16:07 +0000

    Allow user to submit recipe to database

--- app.py
+++ app.py
@@ -100,8 +100,22 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["Get", "POST"])
 def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "ingredients_list": request.form.get("ingredients_list"),
+            "recipe_img": request.form.get("recipe_img"),
+            "prep_time": request.form.get("prep_time"),
+            "instructions": request.form.get("instructions"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Successfully Added")
+        return redirect(url_for("get_recipes"))
+        
     categories = mongo.db.categories.find().sort("category_name")
     return render_template("add_recipe.html", categories=categories)
 
