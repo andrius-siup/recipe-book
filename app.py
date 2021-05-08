ada3f65d5cd51ea3c63e6dd090d342f7c920aaff
andrius-siup <andrius.siupinys@gmail.com>
2021-05-08 18:14:14 +0000

    Add search box and functionality, that search same word in recipe_name and ingredients_list

--- app.py
+++ app.py
@@ -26,6 +26,14 @@ def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
+
 # register
 
 
