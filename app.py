17ae3d9ee352a260a6c9b2c5626b83b5f98d7a8a
andrius-siup <andrius.siupinys@gmail.com>
2022-11-15 11:30:40 +0000

    Deployed at render - moved os below flask and flask_pymongo

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
 from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 if os.path.exists("env.py"):
     import env
 
